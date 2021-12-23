68221804bba581a1162fd2130b13d8016ea9f4c3
Sunil Islam <dev@sunil.sh>
2021-12-22 16:18:08 -0800

    test: update tests to include new validation types

--- tests/validation.js
+++ tests/validation.js
@@ -23,8 +23,8 @@ describe("Testing name validation.", function () {
   it("admin should return as false.", function () {
     assert.equal(Validation.name("admin").valid, false);
   });
-  it("a a should return as false.", function () {
-    assert.equal(Validation.name("a a").valid, false);
+  it("a name should return as false.", function () {
+    assert.equal(Validation.name("a name").valid, false);
   });
 });
 
@@ -37,5 +37,64 @@ describe("Testing password validation.", function () {
   });
   it("pass123 should return as false.", function () {
     assert.equal(Validation.password("pass123").valid, false);
+  });
+});
+
+describe("Testing number validation.", function () {
+  it("123 should return as true.", function () {
+    assert.equal(Validation.number("123").valid, true);
+  });
+  it("3c should return as false.", function () {
+    assert.equal(Validation.number("3c").valid, false);
+  });
+  it("3. should return as false.", function () {
+    assert.equal(Validation.number("3.").valid, false);
+  });
+});
+
+describe("Testing undefined validation.", function () {
+  it("should return a empty payload.", function () {
+    assert.equal(Validation.body([{ name: "test", value: "test" }]).length, 0);
+  });
+  it("should return a payload of one error.", function () {
+    assert.equal(
+      Validation.body([{ name: "test", value: undefined }]).length,
+      1
+    );
+  });
+  it("should return a payload of two errors.", function () {
+    assert.equal(
+      Validation.body([
+        { name: "test", value: undefined },
+        { name: "null", value: null },
+      ]).length,
+      2
+    );
+  });
+});
+
+describe("Testing mixed validation.", function () {
+  it("should return an empty payload", function () {
+    assert.equal(
+      Validation.validate([{ name: "name", value: "test name", type: "name" }])
+        .length,
+      0
+    );
+  });
+  it("should return a payload of one error.", function () {
+    assert.equal(
+      Validation.validate([{ name: "number", value: "3c", type: "number" }])
+        .length,
+      1
+    );
+  });
+  it("should return a payload of two errors.", function () {
+    assert.equal(
+      Validation.validate([
+        { name: "name", value: "test", type: "name" },
+        { name: "float", value: "1.2c", type: "float" },
+      ]).length,
+      2
+    );
   });
 });
